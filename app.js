9392862e3320fc6df4afa0bffb255a75d68b9133
G4totkoco <big.tetuka@gmail.com>
2023-11-28 11:44:12 +0700

    penambahan wallpaper dan sedikit gerakan

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 const app = Vue.createApp
 ({
-
-
   data() 
   {
-    return { hitung: 0, nama: '' };
-  },
+    return { hitung: 0, nama: '' }; },
   
   methods: 
   {
-    clear () { this.hitung = 0 , this.nama = '' , this.inputValue = '' ; },
-	
-	siapa (event, lastName) { this.nama = event.target.value + ' ' + lastName; },
+    clear ()
+    {
+      this.hitung = 0 , this.nama = '' , this.inputValue = '' ; },
+
     
-	devide (num) { this.hitung /= num; },
-	
-	multiply (num) { this.hitung *= num; },
-	
-	add (num) { this.hitung += num; },
-	
-	reduce (num) { this.hitung -= num; }
+    siapa (event, lastName)
+    {
+      this.nama = event.target.value + ' ' + lastName; },
+
+
+    devide (num)
+    {
+      this.hitung /= num; },
+
+
+    multiply (num)
+    {
+      this.hitung *= num; },
+
+
+    add (num)
+    {
+      this.hitung += num; },
+      
+
+    reduce (num)
+    {
+      this.hitung -= num; } 
   }
-  
-  
 });
 
-app.mount('#events');
+app.mount('#eko');
 
 mrasyid
 diysarm
